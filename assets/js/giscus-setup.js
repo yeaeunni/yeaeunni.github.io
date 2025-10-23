7aee8ef1be1662e0015a2ab432c48db86ae7ec36
yeaeunni <yegong2@illinois.edu>
2025-10-23 16:24:07 +0000

    Deploying to gh-pages from @ yeaeunni/yeaeunni.github.io@0fcb88eb4dbb86b1a417bef7a65fd6d0dec324a5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yeaeunni/yeaeun.github.io",
+    "data-repo": "yeaeunni/yeaeunni.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
